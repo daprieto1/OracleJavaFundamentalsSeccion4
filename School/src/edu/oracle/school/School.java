3fc3a1010c7a6fae531b5f23b621eae19380ceaf
Diego Prieto <da.prieto1@escuelaing.edu.co>
2020-03-05 09:47:49 -0400

    control

--- School/src/edu/oracle/school/School.java
+++ School/src/edu/oracle/school/School.java
@@ -8,23 +8,25 @@ import java.util.Stack;
 import edu.oracle.school.models.*;
 
 public class School {
-	
-	public static void main(String args []) {
-		Person p = new Person("Jorge",'m',12345,new Date(1993,12,23));
-		Student s1 = new Student("Pedro", 'm', 78234, new Date(2002,3,23));
-		Student s2 = new Student("José", 'm', 78234, new Date(2002,3,23), 75, 3, 3000, new String[][] {{"Matemáticas"}});
+
+	public static void main(String args[]) {
+		Person p = new Person("Jorge", 'm', 12345, new Date(1993, 12, 23));
+		Student s1 = new Student("Pedro", 'm', 78234, new Date(2002, 3, 23));
+
+		boolean isValid = false;
+		isValid = s1.setSchoolInfo("primaria", 1, 2);
+		System.out.println(isValid ? "CORRECTO" : "INCORRECTO");
+
+		System.out.println(s1.getLiteralGrade(69));
+		System.out.println(s1.getLiteralGrade(79));
+		System.out.println(s1.getLiteralGrade(89));
+		System.out.println(s1.getLiteralGrade(99));
 		
-		
-		
-		LocalDate fecha = LocalDate.now();		
-		System.out.println(fecha.toString());
-		System.out.println(fecha);
-		System.out.println(p);
-		System.out.println(s1);
-		
-		
-		
-		
+		System.out.println(s1.getGradeMessage('A'));
+		System.out.println(s1.getGradeMessage('B'));
+		System.out.println(s1.getGradeMessage('C'));
+		System.out.println(s1.getGradeMessage('F'));
+
 	}
-	
+
 }
--- School/src/edu/oracle/school/models/Student.java
+++ School/src/edu/oracle/school/models/Student.java
@@ -8,26 +8,32 @@ import java.util.Date;
  * @author diegoprietotorres
  *
  */
-public class Student extends Person{
-	
+public class Student extends Person {
+
 	private double grade;
-	private int level;
+
+	private String level;
+	private int ciclo;
+	private int course;
+
 	private double montlySchoolBill;
-	private String[][] schedule;	
-	
+	private String[][] schedule;
+
 	/**
 	 * Constructor de la super clase
+	 * 
 	 * @param name
 	 * @param genere
 	 * @param cedula
 	 * @param birthDate
 	 */
 	public Student(String name, char genere, long cedula, Date birthDate) {
-		super(name, genere, cedula, birthDate);		
+		super(name, genere, cedula, birthDate);
 	}
-	
+
 	/**
 	 * Constructor del Estudiante.
+	 * 
 	 * @param name
 	 * @param genere
 	 * @param cedula
@@ -37,32 +43,81 @@ public class Student extends Person{
 	 * @param montlySchoolBill
 	 * @param schedule
 	 */
-	public Student(	String name, char genere, long cedula, Date birthDate, 
-					double grade, int level, double montlySchoolBill, String[][] schedule) {
+	public Student(String name, char genere, long cedula, Date birthDate, double grade, String level,
+			double montlySchoolBill, String[][] schedule) {
 		super(name, genere, cedula, birthDate);
 		this.grade = grade;
-		this.level = level;
 		this.montlySchoolBill = montlySchoolBill;
 		this.schedule = schedule;
 	}
-	
+
 	/**
-	 * Modificadores de acceso	 
+	 * Modificadores de acceso
 	 */
-	public void setLevel(int level) {
-		this.level = level;
+	public boolean setSchoolInfo(String level, int ciclo, int course) {
+
+		if (!level.equals("primaria") && !level.equals("secundaria"))
+			return false;
+
+		if (!(ciclo == 1 || ciclo == 2))
+			return false;
+
+		if (ciclo == 1 && !(course >= 1 && course <= 3))
+			return false;
+
+		if (ciclo == 2 && !(course >= 4 && course <= 6))
+			return false;
+
+		return true;
 	}
-	
+
+	public char getLiteralGrade(int grade) {
+		return grade < 70 ? 'F' : (grade < 80 ? 'C' : (grade < 90 ? 'B' : 'A'));
+	}
+
+	public String getGradeMessage(char literalGrade) {
+		/*
+		 * if (literalGrade == 'A') { return "DIRECTOR: El alumno es muy bueno"; } else
+		 * if (literalGrade == 'B') { return "PADRES: TIENEN UN HIJO SOBRESALIEBNTE!"; }
+		 * else if (literalGrade == 'C') { return "ESTUDIANTE: USTED DEBE MEJORAR!"; }
+		 * else { return "PADRES: DEBEN AYUDAR MÁS A SU HIJO!"; }
+		 */
+
+		/*
+		 * return literalGrade == 'F' ? "PADRES: DEBEN AYUDAR MÁS A SU HIJO!" :
+		 * (literalGrade == 'C' ? "ESTUDIANTE: USTED DEBE MEJORAR!" : (literalGrade ==
+		 * 'B' ? "PADRES: TIENEN UN HIJO SOBRESALIEBNTE!" :
+		 * "DIRECTOR: El alumno es muy bueno"));
+		 */
+
+		String message = "";
+		switch (literalGrade) {
+		case 'A':
+			message = "DIRECTOR: El alumno es muy bueno";
+			break;
+		case 'B':
+			message = "PADRES: TIENEN UN HIJO SOBRESALIEBNTE!";
+			break;
+		case 'C':
+			message = "ESTUDIANTE: USTED DEBE MEJORAR!";
+			break;
+		case 'F':
+			message = "PADRES: DEBEN AYUDAR MÁS A SU HIJO!";
+			break;
+		}
+		return message;
+
+	}
+
 	/**
 	 * Funciones públicas
 	 */
 	@Override
 	public String toString() {
 		String result = super.toString();
-				
+
 		result += "\n Datos de Estudiante";
-		result += "\n Level: " + this.level;
-		
+
 		return result;
 	}
 
